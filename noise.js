17a64d1b205139583967da2f08469b28ab355059
Janice Lichtman <jalichtman@gmail.com>
2017-05-22 15:18:48 -0700

    have play by click working

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
-  // your code here
+
+
+  var playByNote = function(musicNote){
+    var musicID = "#"+musicNote+"Audio";
+    $(musicID)[0].load();
+    $(musicID)[0].play();
+  };
+
+
+  $(".instrument").on("click", "button", function(event){
+    var musicTone = $(this).attr("class").substr(-1);
+    playByNote(musicTone);
+  });
 });
