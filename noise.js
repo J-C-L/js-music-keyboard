e3de01d3505cdd8487c802be0b965f680a5a0bc3
Janice Lichtman <jalichtman@gmail.com>
2017-05-22 15:58:01 -0700

    redid keyboard to play using dfghjkl

--- noise.js
+++ noise.js
@@ -7,9 +7,18 @@ $(document).ready( function() {
     $(musicID)[0].play();
   };
 
-
   $(".instrument").on("click", "button", function(event){
     var musicTone = $(this).attr("class").substr(-1);
     playByNote(musicTone);
   });
+
+
+  $("body").on("keydown", function(event){
+    // Keyboard plays using keys: dfghjkl
+    var usedKeys = {'d':'c', 'f':'d', 'g':'e', 'h':'f', 'j':'g', 'k':'a', 'l':'b'};
+    var musicTone = usedKeys[event.key.toLowerCase()];
+
+    playByNote(musicTone);
+  });
+
 });
